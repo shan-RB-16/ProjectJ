0ffa80c603f61b06e16f14df969b6eae569fc5de
unknown <darshan.rb2022cce@sece.ac.in>
2026-07-11 00:31:57 +0530

    hamburger fix

--- src/pages/Hamburger.jsx
+++ src/pages/Hamburger.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./hamburger.css";
+import "./Hamburger.css";
 
 export default function Hamburger() {
   const [open, setOpen] = useState(false);
